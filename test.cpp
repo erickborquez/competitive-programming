cc11856963dcabdf84c6f5e00dcb93cfe3e82dfe
ErickJoestar <erickborquez@gmail.com>
2020-09-14 19:42:52 -0600

    update

--- test.cpp
+++ test.cpp
@@ -1,26 +1,28 @@
-#include <bits/stdc++.h>
-#define ENDL '\n'
-#define deb(u) cout << #u " : " << (u) << ENDL;
-#define deba(alias, u) cout << alias << ": " << (u) << ENDL;
-#define debp(u, v) cout << u << " : " << v << ENDL;
-#define pb push_back
-#define F first
-#define S second
-#define lli long long
-#define pii pair<int, int>
-#define pll pair<lli, lli>
-#define ALL(a) (a).begin(), (a).end()
-#define ALLR(a) (a).rbegin(), (a).rend()
-#define FOR(i, a, n) for (int i = (a); i < (n); ++i)
-#define FORN(i, a, n) for (int i = (a - 1); i >= n; --i)
-#define IO                          \
-  ios_base::sync_with_stdio(false); \
-  cin.tie(0);                       \
-  cout.tie(0)
+#include <ctime>
+#include <iostream>
+#include <unordered_map>
 using namespace std;
+
+const int N = 2e5;
+
+void insert_numbers(long long x)
+{
+  clock_t begin = clock();
+  unordered_map<long long, int> numbers;
+
+  for (int i = 1; i <= N; i++)
+    numbers[i * x] = i;
+
+  long long sum = 0;
+
+  for (auto &entry : numbers)
+    sum += (entry.first / x) * entry.second;
+
+  printf("x = %lld: %.3lf seconds, sum = %lld\n", x, (double)(clock() - begin) / CLOCKS_PER_SEC, sum);
+}
 
 int main()
 {
-  IO;
-  return 0;
+  insert_numbers(107897);
+  insert_numbers(126271);
 }
