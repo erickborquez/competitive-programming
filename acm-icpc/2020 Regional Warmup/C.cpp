6fb2c4c854c1db4906b45d0a9171187de554c6b1
Erick Borquez <erickborquez@gmail.com>
2021-07-03 12:52:14 -0600

    Add ICPC 2020 Regional warmup

---
+++ acm-icpc/2020 Regional Warmup/C.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#define DEBUG true
+#define ENDL '\n'
+#define deb(u) \
+  if (DEBUG)   \
+    cout << #u " : " << (u) << ENDL;
+#define deba(alias, u) \
+  if (DEBUG)           \
+    cout << alias << ": " << (u) << ENDL;
+#define debp(u, v) \
+  if (DEBUG)       \
+    cout << u << " : " << v << ENDL;
+#define pb push_back
+#define F first
+#define S second
+#define lli long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<lli, lli>
+#define ALL(a) (a).begin(), (a).end()
+#define ALLR(a) (a).rbegin(), (a).rend()
+#define FOR(i, a, n) for (int i = (a); i < (n); ++i)
+#define FORN(i, a, n) for (int i = (a - 1); i >= n; --i)
+#define IO                          \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);                       \
+  cout.tie(0)
+using namespace std;
+
+int main()
+{
+  IO;
+  int n, x;
+  cin >> n >> x;
+  int last = -1;
+  int best = 1;
+  int curr = 0;
+  while (n--)
+  {
+    int u;
+    cin >> u;
+    if (last == -1 || u - last > x)
+    {
+      last = u;
+      curr = 1;
+      continue;
+    }
+    curr++;
+    last = u;
+    best = max(best, curr);
+  }
+  cout << best << ENDL;
+  return 0;
+}
